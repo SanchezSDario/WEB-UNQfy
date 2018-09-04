5c7a5b9fb92d789e87b059119b3640817301a0b5
SanchezSDario <dariosebastiansanchez@gmail.com>
2018-09-04 17:11:46 -0300

    Getters y setters de track implementados

--- track.js
+++ track.js
@@ -5,4 +5,36 @@ class Track{
         this.duration = _duration;
         this.genres = _genres;
     }
+
+    getId(){
+        return this.idTrack;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getDuration(){
+        return this.duration;
+    }
+
+    getGenres(){
+        return this.genres;
+    }
+
+    setId(id){
+        this.idTrack = id;
+    }
+
+    setName(name){
+        this.name = name;
+    }
+
+    setDuration(duration){
+        this.duration = duration;
+    }
+
+    setGenres(genres){
+        this.genres = genres;
+    }
 }
