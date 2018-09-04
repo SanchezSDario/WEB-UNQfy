f2de12242bf8d558f915f44a82df49466b1dd62f
SanchezSDario <dariosebastiansanchez@gmail.com>
2018-09-04 17:05:32 -0300

    Getters y setters de album implementados

--- album.js
+++ album.js
@@ -7,4 +7,52 @@ class Album{
         this.genres = _genres;
         this.composers = _composers;
     }
+
+    getId(){
+        return this.idAlbum;
+    }
+
+    getName(){
+        return this.name;
+    }
+
+    getYear(){
+        return this.year;
+    }
+
+    getTracks(){
+        return this.tracks;
+    }
+
+    getGenres(){
+        return this.genres;
+    }
+
+    getComposers(){
+        return this.composers;
+    }
+
+    setId(id){
+        this.idAlbum = id;
+    }
+
+    setName(name){
+        this.name = name;
+    }
+
+    setYear(year){
+        this.year = year;
+    }
+
+    setTracks(tracks){
+        this.tracks = tracks;
+    }
+
+    setGenres(genres){
+        this.genres = genres;
+    }
+
+    setComposers(composers){
+        this.composers = composers;
+    }
 }
